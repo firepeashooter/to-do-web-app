f42188eff13733740c219829918459f2c4934c8c
Ben Braun <benjamin.braun2003@gmail.com>
2025-08-20 19:21:10 -0400

    Add plus button

--- src/scripts/ScreenController.js
+++ src/scripts/ScreenController.js
@@ -1,3 +1,4 @@
+import { add } from "date-fns";
 import { Project } from "./Project.js";
 import { ToDo } from "./ToDo.js";
 
@@ -33,6 +34,11 @@ export class ScreenController{
             main.appendChild(this.renderTodo(project.todos[i]));
 
         }
+
+        const addButton = document.createElement("button");
+        addButton.classList.add("add--button");
+        addButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>';
+        main.appendChild(addButton);
     }
 
     //Returns a complete ToDo Div
