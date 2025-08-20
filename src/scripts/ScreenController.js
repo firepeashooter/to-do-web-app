1b891282642f30331a019eed123b843de5129e70
Ben Braun <benjamin.braun2003@gmail.com>
2025-08-20 19:27:53 -0400

    Style Add Todo Button

--- src/scripts/ScreenController.js
+++ src/scripts/ScreenController.js
@@ -36,7 +36,7 @@ export class ScreenController{
         }
 
         const addButton = document.createElement("button");
-        addButton.classList.add("add--button");
+        addButton.classList.add("add--todo--button");
         addButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6"><path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>';
         main.appendChild(addButton);
     }
